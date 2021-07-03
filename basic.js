9f3738ac3e305ba18854ec19b94017b959dbdf0c
chivenice <chivenice29@gmail.com>
2021-07-03 23:53:04 +0900

    nodejs

---
+++ basic.js
@@ -0,0 +1,25 @@
+console.log("Hello World")
+const a = "const" /* Bien KHONG the thay doi */
+let b = "let" /* Bien CO the thay doi - *scope */
+var c = "var" /* Bien CO the thay doi */
+
+let undefinedTest;
+let nullTest = null;
+
+const name = "Chi"
+const number = "1"
+const obj = {
+    firstName :"Nguyen",
+    lastName : "Chi",
+    age : "23",
+    male: true,
+}
+// Data Types
+// String : text
+// Number 
+// Object : luu tru nhung du lieu khac nhau
+// Boolean : True or False
+// Null, Undefined  assign: gan gia tri
+
+
+console.log({obj, undefinedTest, nullTest})
